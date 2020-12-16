f7cad2296c671960df7ae786b94e6836fb052666
kot26-7 <taro726896oka@gmail.com>
2020-12-16 12:48:14 +0900

    set up for materialize and create carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,28 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
-
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+$(document).ready(function(){
+  $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+  autoplay()
+  function autoplay() {
+    $('.carousel.carousel-slider').carousel('next');
+    autoplay: true,
+    setTimeout(autoplay, 7500);
+  }
+});
