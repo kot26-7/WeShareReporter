e77ccf8c23340b542c96fc4d726bb0591e35cdd3
kot26-7 <taro726896oka@gmail.com>
2020-12-17 13:24:17 +0900

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0-9_]+\z/.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { maximum: 50 },
+                       format: { with: VALID_USERNAME_REGEX }
+  validates :postal_code, presence: true
+  validates :prefecture_code, presence: true
+  validates :city, presence: true
 end
